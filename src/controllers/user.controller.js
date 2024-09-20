10f764e778d79045284b9564ae4d706a2b98cfe7
satyam-006 <satyamyt876@gmail.com>
2024-09-20 15:52:20 +0530

    added comment,like,tweet,playlist model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 
 const generateAccessAndRefreshTokens = async (userId) => {
@@ -205,7 +206,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 })
 
-const changeCurrentUser = asyncHandler(async (req, res) => {
+const changeCurrentPassword = asyncHandler(async (req, res) => {
     const { oldPassword, newPassword } = req.body
 
 
@@ -226,7 +227,9 @@ const changeCurrentUser = asyncHandler(async (req, res) => {
 })
 
 const getCurrentUser = asyncHandler(async (req, res) => {
-    return res.status(200).json(200, req.user, "current user fetched successfully")
+    return res.status(200).json(
+        new ApiResponse(200, req.user, "User retrieved successfully")
+    )
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -248,7 +251,9 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
         { new: true }
     ).select("-password")
 
-    return res.status(200).json(200, user, "Account details updated successfully")
+    return res.status(200).json(
+        new ApiResponse(200, user, "Account details updated successfully")
+    )
 })
 
 const updateUserAvatar = asyncHandler(async (req, res) => {
@@ -276,7 +281,9 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
         }
     ).select("-password")
 
-    return res.status(200).json(200, user, "Avatar updated successfully")
+    return res.status(200).json(new ApiResponse(
+        200, avatar.url, "Avatar updated successfully"
+    ))
 })
 
 const updateUserCoverImage = asyncHandler(async (req, res) => {
@@ -304,7 +311,141 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
         }
     ).select("-password")
 
-    return res.status(200).json(200, user, "cover image updated successfully")
+    return res.status(200).json(
+        new ApiResponse(
+            200, coverImage.url, "Cover image updated successfully"
+        )
+    )
+})
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params
+    
+    if (!username?.trim) {
+        throw new ApiError(400, "Username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subcribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$subcribers.subscriber"] },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ])
+    
+    if (!channel?.length) {
+        throw new ApiError(404, "Channel dose not exists")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                channel[0],
+                "Channel fetched successfully"
+            )
+        )
+})
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id:new mongoose.Types.ObjectId(req.user._id)
+
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                user[0].watchHistory,
+                "Watch history fetched successfully"
+            )
+        )
 })
 
 export {
@@ -312,9 +453,11 @@ export {
     loginUser,
     logoutUser,
     refreshAccessToken,
-    changeCurrentUser,
+    changeCurrentPassword,
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 }
