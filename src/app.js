23b17834dfaed6c4c4213a6cff335ad2c061886d
satyam-006 <satyamyt876@gmail.com>
2024-10-02 11:21:17 +0530

    all routes import

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(cors(
         credentials:true
     }
 ));
+
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
@@ -18,9 +19,24 @@ app.use(cookieParser());
 
 // routes import
 import userRoute from './routes/user.routes.js'
+import videoRoute from './routes/video.routes.js'
+import commentRoute from './routes/comment.routes.js'
+import playlistRoute from './routes/playlist.routes.js'
+import likeRoute from './routes/like.routes.js'
+import subscriptionRoute from './routes/subscription.routes.js'
+import tweetRoute from './routes/tweet.routes.js'
+import dashboardRoute from './routes/dashboard.routes.js'
+import healthcheckRoute from './routes/healthcheck.routes.js'
 
 // routes declaration
 app.use("/api/v1/users",userRoute)
-
+app.use("/api/v1/videos",videoRoute)
+app.use("/api/v1/comments",commentRoute)
+app.use("/api/v1/playlists",playlistRoute)
+app.use("/api/v1/likes",likeRoute)
+app.use("/api/v1/subscriptions",subscriptionRoute)
+app.use("/api/v1/tweets",tweetRoute)
+app.use("/api/v1/dashboard",dashboardRoute)
+app.use("/api/v1/healthcheck",healthcheckRoute)
 
 export { app }
